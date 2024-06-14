b01ab46deaff1776f776de4f66fad945d8145f27
RawanAsad <asad.rawan.2003@gmail.com>
2024-06-14 17:15:37 +0300

    my code

---
+++ lib/features/bloc/post_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'post_event.dart';
+part 'post_state.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  final GetPostsUseCase _getPostsUseCase;
+
+  PostBloc(this._getPostsUseCase) : super(PostInitial()) {
+    on<GetPosts>(
+      (event, emit) async {
+        final result = await _getPostsUseCase.call();
+
+        result.fold((error) {
+          emit(PostError(errorMessage: error.errorMessage));
+        }, (posts) {
+          emit(
+            PostSuccess(
+              posts: posts,
+            ),
+          );
+        });
+      },
+    );
+  }
+}
